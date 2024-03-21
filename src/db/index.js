25ab5b1584059ed7b767918c56f8af3a66146749
Akash <thakare.akash007@gmail.com>
2024-03-21 14:27:11 +0530

    create server add cors and cookieParser

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
-const connectDB = async () =>{
-    try{
-       const connectInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-       console.log(`\n Mongoose Connect !!DB HOST: ${
-        connectInstance.connection.host
-       }`)
-    }
-    catch (error){
-        console.log("MongoDB connecting error",error);
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `\n Mongoose Connect !!DB HOST: ${connectInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connecting error", error);
+    process.exit(1);
+  }
+};
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 
-connectDB();
+connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log( `server listening on ${process.env.PORT}`)
+    })
+})
+.catch( (err) =>{
+    console.log("MONGO db connection failed !!",err)
+})
