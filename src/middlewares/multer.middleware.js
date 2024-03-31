8c09be38b7129d7bb6a18ac0c9d6576062980a82
Akash <thakare.akash007@gmail.com>
2024-03-31 10:44:24 +0530

    update code

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,13 +1,19 @@
+// Import multer for handling file uploads
 import multer from "multer";
 
+// Define storage configuration for multer
 const storage = multer.diskStorage({
+  // Set the destination directory for uploaded files
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "./public/temp"); // Destination directory is "./public/temp"
   },
+  // Set the filename for uploaded files
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
-    
+    cb(null, file.originalname); // Use the original filename for storing the uploaded file
   },
 });
 
+// Create a multer instance with the defined storage configuration
 export const upload = multer({ storage });
+
+// Export the upload function as a named export
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,25 +1,36 @@
+// Define Cloudinary configuration using environment variables
+// Import necessary modules
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+// Configure Cloudinary with environment variables
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NANE,
-  api_key: process.env.API_KEY_CLOUDINARY,
-  api_secret: process.env.API_SECRET_CLOUDINARY,
+  cloud_name: process.env.CLOUD_NANE, // Cloud name from environment variables
+  api_key: process.env.API_KEY_CLOUDINARY, // API key from environment variables
+  api_secret: process.env.API_SECRET_CLOUDINARY, // API secret from environment variables
 });
 
+// Define an asynchronous function to upload files to Cloudinary
 const uploadOnCloudinary = async (localFilePath) => {
   try {
+    // Check if localFilePath is provided
     if (!localFilePath) return null;
+
+    // Upload the file to Cloudinary and get the upload result
     const uploadResult = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
+      resource_type: "auto", // Resource type for upload (auto-detect)
     });
-    console.log("file is uploaded on cloudinary ", uploadResult.url);
+
+    // Log the upload success and return the uploaded file URL
+    console.log("File uploaded on Cloudinary: ", uploadResult.url);
     return uploadResult.url;
   } catch (error) {
-    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload
+    // Handle upload errors
+    fs.unlinkSync(localFilePath); // Remove the locally saved temporary file as the upload failed
     console.log(error);
-    return null;
+    return null; // Return null in case of upload failure
   }
 };
 
+// Export the uploadOnCloudinary function as the default export
 export default uploadOnCloudinary;
