3814b92fa131ece40d0b6f2d671eba82eac80059
Akash <thakare.akash007@gmail.com>
2024-04-01 22:19:41 +0530

    all setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         massage = "Something want wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ) {
         super(massage);
         this.statusCode = statusCode;
@@ -12,8 +12,8 @@ class ApiError extends Error{
         this.massage = massage;
         this.success = false
 
-        if(statck){
-            this.stack = statck
+        if(stack){
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constrictor)
         }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
 const asyncHandler = (requestHandler) => {
-  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
 };
 
 export { asyncHandler };
