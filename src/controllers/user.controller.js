f289d761cdfa1b8038463793894303c80f6cecfe
Akash <thakare.akash007@gmail.com>
2024-04-21 23:55:33 +0530

    fix logout bugs and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -150,8 +150,8 @@ const logOutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     { new: true }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,14 +36,14 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("current-user").get(verifyJWT, getCurrentuser);
+router.route("/current-user").get(verifyJWT, getCurrentuser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar-upload")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/cover-iamge")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/watch-history").get(verifyJWT, getWatchHistory);
 export default router;
